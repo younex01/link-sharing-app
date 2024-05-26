b5a5e3db5677456bbc236d07fc5f66af925dc4d5
Youness Elousse <yelousse@e1r5p11.1337.ma>
2024-05-26 23:02:56 +0100

    add: drag and drop

--- src/main.tsx
+++ src/main.tsx
@@ -40,9 +40,9 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  
     <ApolloProvider client={client}>
     <RouterProvider router={router} />
     </ApolloProvider>
-  </React.StrictMode>,
+,
 )
