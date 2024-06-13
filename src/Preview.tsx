b9463081b6bb27684eb7d3d93dcbea89de942076
younex01 <younesyounex1@gmail.com>
2024-06-13 10:16:36 +0100

    fix : preview page

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -33,10 +33,9 @@ const Preview = () => {
   const [isCopied, setIsCopied] = useState(false);
 
 	const handleClick = async () => {
-    const domain = import.meta.env.VITE_REACT_APP_DOMAIN_PREVIEW;
 		try {
 			await navigator.clipboard.writeText(
-				`${domain}${dataProfile.profile[0].id}`
+				`${dataProfile.profile[0].id}`
 			);
 			setIsCopied(true);
 
