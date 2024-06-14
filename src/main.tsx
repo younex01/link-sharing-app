269c7e4f25793567881c5552a21c1a52b69b53eb
younex01 <younesyounex1@gmail.com>
2024-06-14 13:11:00 +0100

    add:change uri

--- src/main.tsx
+++ src/main.tsx
@@ -57,7 +57,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     domain={domain}
     clientId={clientId}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: 'https://link-sharing-app-ebon.vercel.app'
     }}
   >
     
