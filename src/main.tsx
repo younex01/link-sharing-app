e8c995993b2122f81f292f525a984b61df2b1387
younex01 <younesyounex1@gmail.com>
2024-06-18 14:10:18 +0100

    add: guard to home page

--- src/main.tsx
+++ src/main.tsx
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/home",
-    element: <Home />,
+    element: <AuthenticationGuard component={Home} />,
     errorElement: <ErrorPage />,
   },
   {
